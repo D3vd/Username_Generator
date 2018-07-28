7e9d71e964f8469cbf016bb332b6f1ab9a7636dd
Dev-C137 <dev.91096@gmail.com>
2018-07-28 08:33:00 +0530

    Added two name modifiers

--- username_generator.py
+++ username_generator.py
@@ -24,12 +24,35 @@ def create_username():
 
     first_name, last_name = get_names()
 
-    op = random.choice(range(2))
+    op = random.choice(range(4))
 
     if op == 0:
         return number_last(first_name, last_name)
     if op == 1:
         return number_mid(first_name, last_name)
+    if op == 2:
+        return first_name_number(first_name)
+    if op == 3:
+        return last_name_number(last_name)
+
+
+def last_name_number(last_name):
+
+    username = ''
+    n = random.choice(range(1, 4))
+    username += last_name
+    username += ''.join(random.choice(string.digits) for _ in range(n))
+
+    return username.replace(' ', '')
+
+def first_name_number(first_name):
+
+    username = ''
+    n = random.choice(range(1, 4))
+    username += first_name
+    username += ''.join(random.choice(string.digits) for _ in range(n))
+
+    return username.replace(' ', '')
 
 
 def number_last(first_name, last_name):
